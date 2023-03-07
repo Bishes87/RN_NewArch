3ab24417e4fcac71019b2c79250a09577ad63e17
Bishes87 <bishes.sahaofficial@gmail.com>
2023-03-07 17:26:46 +0530

    Project Files Added

---
+++ windows/RN_NewArch/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::RN_NewArch::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::RN_NewArch::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
